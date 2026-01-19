e008855d6916b9557212bc495758e9c38d0211a8
KennOtieno <otiken001@gmail.com>
2026-01-19 16:12:36 +0300

    Users

---
+++ tests/users.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { request } from 'node:http';
+
+test('Status code is 200', async ({ request }) => {
+  // GET a request from users endpoint and check if it's status code is 200
+  const response = await request.get('https://jsonplaceholder.typicode.com/users');
+  await expect (response.status()).toBe(200);
+});
+
+// The next test entails figuring out if the response is an array and if the array length is 100
+
+test('Response is an array with a length of 100', async ({ request}) => {
+
+
+  const response = await request.get('https://jsonplaceholder.typicode.com/users');
+  const responseBody = await response.json();
+  expect (Array.isArray(responseBody)).toBe(true);
+  expect (responseBody).toHaveLength(100);
+
+
+});
