9390e6be525e00560df5c1c591e43e7d093a78e4
KennOtieno <otiken001@gmail.com>
2026-01-19 12:54:11 +0300

    In progress

--- tests/posts.spec.ts
+++ tests/posts.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Status code is 200', async ({ request }) => {
-  // My thinking is to: 
-  // 1.) Send a GET Request to the API /posts endpoint
-  // 2.) If it comes up with a json that shows userId, title, body, etc, I'll have to print status code 200
-  const response = await request.get('https://jsonplaceholder.typicode.com/posts')
-  const responseObject = await response.json()
-  console.log(responseObject)
+  // GET a request from posts endpoint and check if it's status code is 200
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts');
+  await expect (response.status()).toBe(200);
 });
+
+// The next test entails figuring out if the response is an array and if the array length is 100
+
 
