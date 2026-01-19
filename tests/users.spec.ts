7830faf8fa99cf264fcc335e64fc854179b63bfe
KennOtieno <otiken001@gmail.com>
2026-01-19 20:28:13 +0300

    users

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -7,7 +7,7 @@ test('Status code is 200', async ({ request }) => {
   await expect (response.status()).toBe(200);
 });
 
-// The next test entails figuring out if the response is an array and if the array length is 100
+// The next test entails figuring out if the response is an array and if the array length is 10
 
 test('Response is an array with a length of 100', async ({ request}) => {
 
@@ -15,7 +15,7 @@ test('Response is an array with a length of 100', async ({ request}) => {
   const response = await request.get('https://jsonplaceholder.typicode.com/users');
   const responseBody = await response.json();
   expect (Array.isArray(responseBody)).toBe(true);
-  expect (responseBody).toHaveLength(100);
+  expect (responseBody).toHaveLength(10);
 
 
 });
